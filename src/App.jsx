d970577b6aa5e00b594f4ba84840dbe2eb2265d3
ViktoryiaShylko <vika.shylko@mail.ru>
2023-02-19 17:22:56 +0300

    added themes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import "./App.css";
+import style from "./App.css";
 import Tariff from "./components/Tariff";
 import blueTheme from "./styles/blue.module.scss";
 import greenTheme from "./styles/green.module.scss";
@@ -8,36 +8,37 @@ import grayTheme from "./styles/gray.module.scss";
 const cards = [
   {
     rate: "300",
+    price: "300",
     speed: "10",
   },
   {
     rate: "450",
+    price: "450",
     speed: "50",
   },
   {
     rate: "550",
+    price: "550",
     speed: "100",
-    isSelected: "selected",
   },
   {
     rate: "1000",
+    price: "1000",
     speed: "200",
   },
 ];
 
-const themes = [blueTheme, greenTheme, redTheme, grayTheme];
-
 export default function App() {
+  const themes = [blueTheme, greenTheme, redTheme, grayTheme];
   return (
-    <div className="App">
-      <div className="cards">
+    <div className={style.App}>
+      <div className={style.cards}>
         {cards.map((card, i) => (
           <Tariff
             rate={card.rate}
-            price={card.rate}
+            price={card.price}
             speed={card.speed}
             theme={themes[i]}
-            isSelected={card.isSelected}
           ></Tariff>
         ))}
       </div>
--- src/components/Tariff.jsx
+++ src/components/Tariff.jsx
@@ -1,11 +1,9 @@
-import style from "../styles/card.scss";
-import icon from "../images/icon.png";
+import style from "./style.module.scss";
 
 export default function Tariff(props) {
-  //   const classCard = props.isSelected ? "selected" : "";
   return (
     <div className={style.card}>
-      <header className={style.card__header}>Безлимитный{props.rate}</header>
+      <header className={style.card__header}>Безлимитный {props.rate}</header>
       <div className={style.card__info}>
         <p className={style.card__price}>руб</p>
         <p className={style.card__rate}>{props.rate}</p>
@@ -13,9 +11,8 @@ export default function Tariff(props) {
       </div>
       <div className={style.card__speed}>До {props.speed} Мбит/сек</div>
       <footer className={style.card__footer}>
-        Объём включенного трафика не ограничен
+        Объeм включенного <br /> трафика не ограничен
       </footer>
-      <img src={icon} className={style.card__icon} />
     </div>
   );
 }
