9814cb8b9598795ff051efad4b4d267a8a44dc38
ViktoryiaShylko <vika.shylko@mail.ru>
2023-02-19 17:48:50 +0300

    fixed tariff.jsx

--- src/components/Tariff.jsx
+++ src/components/Tariff.jsx
@@ -6,7 +6,7 @@ export default function Tariff(props) {
       <header className={style.card__header}>Безлимитный {props.rate}</header>
       <div className={style.card__info}>
         <p className={style.card__price}>руб</p>
-        <p className={style.card__rate}>{props.rate}</p>
+        <p className={style.card__rate}>{props.price}</p>
         <p className={style.card__period}>/мес</p>
       </div>
       <div className={style.card__speed}>До {props.speed} Мбит/сек</div>
