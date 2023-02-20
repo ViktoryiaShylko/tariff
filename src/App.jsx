2b7c7585c819d6a10f7f15c632cfb1e93afca079
ViktoryiaShylko <vika.shylko@mail.ru>
2023-02-20 14:23:53 +0300

    added selected class

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,50 @@
 import style from "./App.css";
 import Tariff from "./components/Tariff";
-import blueTheme from "./styles/blue.module.scss";
-import greenTheme from "./styles/green.module.scss";
-import redTheme from "./styles/red.module.scss";
-import grayTheme from "./styles/gray.module.scss";
 
 const cards = [
   {
     rate: "300",
     price: "300",
     speed: "10",
+    colorHeader: "blue-header",
+    colorInfo: "blue-info",
   },
   {
     rate: "450",
     price: "450",
     speed: "50",
+    colorHeader: "green-header",
+    colorInfo: "green-info",
   },
   {
     rate: "550",
     price: "550",
     speed: "100",
+    colorHeader: "red-header",
+    colorInfo: "red-info",
+    isSelected: "selected",
   },
   {
     rate: "1000",
     price: "1000",
     speed: "200",
+    colorHeader: "gray-header",
+    colorInfo: "gray-info",
   },
 ];
 
 export default function App() {
-  const themes = [blueTheme, greenTheme, redTheme, grayTheme];
   return (
     <div className={style.App}>
       <div className={style.cards}>
-        {cards.map((card, i) => (
+        {cards.map((card) => (
           <Tariff
             rate={card.rate}
             price={card.price}
             speed={card.speed}
-            theme={themes[i]}
+            colorHeader={card.colorHeader}
+            colorInfo={card.colorInfo}
+            isSelected={card.isSelected}
           ></Tariff>
         ))}
       </div>
--- src/components/Tariff.jsx
+++ src/components/Tariff.jsx
@@ -2,11 +2,15 @@ import style from "./style.module.scss";
 
 export default function Tariff(props) {
   return (
-    <div className={style.card}>
-      <header className={style.card__header}>Безлимитный {props.rate}</header>
-      <div className={style.card__info}>
-        <p className={style.card__price}>руб</p>
-        <p className={style.card__rate}>{props.price}</p>
+    <div
+      className={`${style.card} ${style[props.isSelected ? "selected" : ""]}`}
+    >
+      <header className={`${style.card__header} ${style[props.colorHeader]}`}>
+        Безлимитный {props.rate}
+      </header>
+      <div className={`${style.card__info} ${style[props.colorInfo]}`}>
+        <p className={style.card__currency}>руб</p>
+        <p className={style.card__price}>{props.price}</p>
         <p className={style.card__period}>/мес</p>
       </div>
       <div className={style.card__speed}>До {props.speed} Мбит/сек</div>
